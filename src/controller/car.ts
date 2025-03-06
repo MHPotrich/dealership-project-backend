74037d98120cfbc84a409b6bd0e4ecc9cb326165
Matheus H. Potrich <matheushp.com@hotmail.com>
2025-03-05 21:11:55 -0300

    car controller

--- src/controller/car.ts
+++ src/controller/car.ts
@@ -1,10 +1,21 @@
+import type { BunRequest } from "bun";
 import { getDatabaseInstance } from "../database";
+import { getResponseNotFound } from "../utils";
 
 const CARS_DB_TABLE = "car";
 
-export function getCars(request) {
-	const LIMIT = request.query.limit || 0;
-	const OFFSET = request.query.offset || 0;
+function getQueryNumber(url: URL, target: string): number {
+	let value: string | null = url.searchParams.get(target);
+
+	if (value) return parseInt(value);
+
+	return 0;
+}
+
+export function getCars(request: BunRequest) {
+	const THIS_URL = new URL(request.url);
+	const LIMIT: number = getQueryNumber(THIS_URL, "limit");
+	const OFFSET: number = getQueryNumber(THIS_URL, "offset");
 	let dbQuery = `SELECT * FROM ${CARS_DB_TABLE}`;
 
 	if (LIMIT > 0 && OFFSET > 0) {
@@ -16,12 +27,50 @@ export function getCars(request) {
 	});
 }
 
-export function getCar() {}
+export function getCar(request: BunRequest) {
+	if (!request.params.id) return getResponseNotFound();
 
-export function addCar() {
-	const NEW_CAR = {};
+	return Response.json(
+		getDatabaseInstance()
+			.query(`SELECT * FROM ${CARS_DB_TABLE} WHERE id = ?`)
+			.get(request.params.id)
+	);
 }
 
-export function updateCar() {}
+export async function addCar(request: BunRequest) {
+	const REQUEST_BODY = await request.json();
 
-export function deleteCar() {}
+	if (!REQUEST_BODY.name) return getResponseNotFound();
+
+	getDatabaseInstance()
+		.query(`INSERT INTO ${CARS_DB_TABLE} (name) VALUES (?)`)
+		.run(REQUEST_BODY.name);
+
+	return new Response(null, { status: 201 });
+}
+
+export async function updateCar(request: BunRequest) {
+	const REQUEST_BODY: object = await request.json();
+	const TARGET_ID = parseInt(request.params.id);
+	const DATABASE_INSTANCE = getDatabaseInstance();
+
+	for (const [key, value] of Object.entries(REQUEST_BODY)) {
+		if (key !== "id") {
+			DATABASE_INSTANCE.query(
+				`UPDATE ${CARS_DB_TABLE} SET ${key} = ? WHERE id = ?`
+			).run(value, TARGET_ID);
+		}
+	}
+
+	return new Response(null, { status: 201 });
+}
+
+export function deleteCar(request: BunRequest) {
+	const TARGET_ID = request.params.id;
+
+	getDatabaseInstance()
+		.query(`DELETE FROM ${CARS_DB_TABLE} WHERE id = ?`)
+		.get(TARGET_ID);
+
+	return new Response(null, { status: 201 });
+}
